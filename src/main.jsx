289ee5944f2ea92b5832ed42eda9654327b1232c
rawillia9801 <163735923+rawillia9801@users.noreply.github.com>
2026-05-21 17:52:36 -0400

    Add React render entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,6 @@
-console.log("Southwest Virginia Chihuahua dashboard scaffold");
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
+createRoot(rootElement).render(<App />);
